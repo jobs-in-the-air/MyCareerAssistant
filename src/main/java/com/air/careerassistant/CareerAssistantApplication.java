96382870a946a9019d4006dca92b9df6a5029ce2
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-10-09 14:39:41 -0700

    spring commit

---
+++ src/main/java/com/air/careerassistant/CareerAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.air.careerassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerAssistantApplication.class, args);
+	}
+
+}
