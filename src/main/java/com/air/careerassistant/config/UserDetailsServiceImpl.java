c0d3a1494dc2f2f76bedba6e4db10c6904803383
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-10-09 15:52:49 -0700

    create usermodel

---
+++ src/main/java/com/air/careerassistant/config/UserDetailsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.air.careerassistant.config;
+
+import com.air.careerassistant.model.user.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserRepository.findByUsername(username);
+    }
+}
---
+++ src/main/java/com/air/careerassistant/model/user/ApplicationUser.java
@@ -0,0 +1,77 @@
+package com.air.careerassistant.model.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class ApplicationUser implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique=true)
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String password;
+
+    public ApplicationUser() {
+
+    }
+
+    public ApplicationUser(String username, String firstname, String lastname, String password) {
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true ;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
