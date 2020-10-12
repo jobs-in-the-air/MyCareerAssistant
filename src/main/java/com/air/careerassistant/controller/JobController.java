8244881de5a02de7068ac554ad9f7fe43b93c602
Meghan Domeck <mdomeck@aol.com>
2020-10-12 18:12:29 -0400

    ready to merge

--- src/main/java/com/air/careerassistant/controller/JobController.java
+++ src/main/java/com/air/careerassistant/controller/JobController.java
@@ -4,12 +4,16 @@ import com.air.careerassistant.model.job.Job;
 import com.air.careerassistant.model.job.JobRepository;
 import com.air.careerassistant.model.jobTrack.JobStatus;
 import com.air.careerassistant.model.jobTrack.JobStatusRepository;
+import com.air.careerassistant.model.user.ApplicationUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 @Controller
 public class JobController {
@@ -46,4 +50,6 @@ public class JobController {
 
         return "details";
     }
+
+
 }
