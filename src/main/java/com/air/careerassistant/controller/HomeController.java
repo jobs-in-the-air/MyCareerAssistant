9005d980ff0627e7e7e320f7df72f512216d87ef
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-10-11 18:37:46 -0700

    connected /jobsearch

--- src/main/java/com/air/careerassistant/controller/HomeController.java
+++ src/main/java/com/air/careerassistant/controller/HomeController.java
@@ -2,10 +2,20 @@ package com.air.careerassistant.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
 @Controller
 public class HomeController {
 @GetMapping("/")
     public String showhome(){
     return "home";
 }
+
+@PostMapping("/jobsearch")
+    public RedirectView showSearches(String title, String location){
+        System.out.println("Inside /job searches here with title "+title+" "+location);
+        return new RedirectView("/");
 }
+}
+
